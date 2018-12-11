5082e1bce1bcdaf9adb4ce25f5426d17159c9a5b
Hugo <lebaherhugo@gmail.com>
2018-12-11 19:34:33 +0100

    artifact configuration

--- src/eu/swnw/Main.java
+++ src/eu/swnw/Main.java
@@ -6,24 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            Grid g = Grid.gridFromFile("data/exemple.txt").calculateSums();
+        if(args.length>0){
+            try {
+                for (String s: args) {
+                    System.out.println(s);
+                }
+                Grid g = Grid.gridFromFile(args[0]).calculateSums();
 
-            NetworkMin g1 = g.constructionEtape4();
-            System.out.println(g1);
-            NetworkRequest g2 = NetworkMin.constructionEtape3(g1);
-            System.out.println(g2);
-            Network g3 = Network.constructionEtape2(g2);
-            System.out.println(g3);
-            NetworkPushRelabel pr = NetworkPushRelabel.networkToNPR(g3);
-            pr.preflot();
-            System.out.println(NetworkPushRelabel.NPRtoNetwork(pr));
-            g.print();
-            System.out.println();
-            g.printResult(NetworkPushRelabel.NPRtoNetwork(pr));
-            int o = 0;
-        } catch (Exception e) {
-            e.printStackTrace();
+                NetworkMin g1 = g.constructionEtape4();
+                NetworkRequest g2 = NetworkMin.constructionEtape3(g1);
+                Network g3 = Network.constructionEtape2(g2);
+                NetworkPushRelabel pr = NetworkPushRelabel.networkToNPR(g3);
+                Network network = NetworkPushRelabel.NPRtoNetwork(pr);
+
+                g.print();
+
+                if(args.length>1 && args[1].equals("--debug")){
+                    System.out.println(g1);
+                    System.out.println(g2);
+                    System.out.println(g3);
+                    pr.preflot();
+                }
+
+                g.printResult(network);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("usage: java -jar <jar> <path> [--debug]");
         }
 
     }
